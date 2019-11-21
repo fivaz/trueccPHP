5dc08e26b43cdf4ea21a1e2cb86f2fd616945e8b
Fivaz Stefane (HES) <stefane.fivaz@hes.adhes.hesge.ch>
2019-11-21 15:30:23 +0100

    a

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 				<option value="client">client</option>
 				<option value="conseiller">conseiller</option>
 			</select>
-			<input type="text" name="name" placeholder="nom">
-			<input type="text" name="pays" placeholder="pays">
-			<input type="text" name="page" placeholder="page">
+			<input type="text" name="nom" placeholder="nom" value="L">
+			<input type="text" name="pays" placeholder="pays" value="Rus">
+			<input type="text" name="page" placeholder="page" value="1">
 			<label>format</label>
 			<select name="format">
 				<option value="">selectioner une option</option>
--- recherche.php
+++ recherche.php
@@ -1,11 +1,35 @@
 <?php
-	function chercher_clients($nom, $pays, $page){
+	function chercher_clients($nom, $pays){
 			$dsn = "mysql:host=localhost;dbname=basescc";
 			$user = "root";
 			$passwd = "";
 			$pdo = new PDO($dsn, $user, $passwd);
       $query = 'SELECT * from client';
+      if($nom || $pays){
+        $query .= " WHERE 1 = 1 ";
+        if($nom){
+          $query .= "AND nom LIKE '{$nom}%' ";
+        }
+        if($pays){
+          $query .= "AND pays LIKE '{$pays}%' ";
+        }
+      }
+      // echo $query;
 	    $stm = $pdo->query($query);
+      return $stm->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  function chercher_conseiller($nom){
+      $dsn = "mysql:host=localhost;dbname=basescc";
+      $user = "root";
+      $passwd = "";
+      $pdo = new PDO($dsn, $user, $passwd);
+      $query = 'SELECT * from conseiller';
+      if($nom){
+          $query .= " WHERE nom LIKE '{$nom}%'";
+      }
+      // echo $query;
+      $stm = $pdo->query($query);
       return $stm->fetchAll(PDO::FETCH_ASSOC);
   }
 
@@ -26,10 +50,72 @@
     if(isset($_POST['format']))
   $format = $_POST['format'];
 
-  if($recherche == "client")
-    $rows = chercher_clients($nom, $pays, $page);
-  else if($recherche == "conseiller")
-    $rows = chercher_conseiller($nom, $page);
+  if($recherche == "client"){
+    $rows = chercher_clients($nom, $pays);
+    $new_rows = get_page($rows, $page);
 
-  print_r($rows);
+   if ($format == "pdf")
+       printPDF($new_rows);
+   else if ($format == "csv")
+       printCSV($new_rows);
+   else if ($format == "xml")
+       printXML($new_rows);
+   else
+       printHTML($new_rows);
+  }
+  else if($recherche == "conseiller"){
+    $rows = chercher_conseiller($nom);
+    $new_rows = get_page($rows, $page);
+    printJSON($new_rows);
+  }
+
+  function get_page($rows, $page){
+    $new_array = array_chunk($rows, 20);
+    return $new_array[$page - 1];
+  }
+
+  function printHTML($rows){
+    foreach ($rows as $row) {
+      echo "<div>".$row['nom']." - ".$row['prenom']." - ".$row['pays']."</div>";
+    }
+  }
+
+  function printCSV($rows){
+    $f = fopen('php://memory', 'w');
+    fwrite($f, "id; nom; prenom; email; pays; npa; conseiller_id;\n");
+    foreach ($rows as $row) {
+        fputcsv($f, $row, ";");
+    }
+    fseek($f, 0);
+    header('Content-Type: application/csv');
+    header('Content-Disposition: attachment; filename="fichier.csv";');
+    fpassthru($f);
+  }
+
+  function printPDF($rows){
+    require('fpdf.php');
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial', '', 12);
+    $pdf->Cell(20, 10, "Nom", 1);
+    $pdf->Cell(20, 10, "Prénom", 1);
+    $pdf->Cell(20, 10, "Pays", 1);
+    $pdf->Cell(20, 10, "NPA", 1, 1);
+    for ($i = 0; $i < count($rows); $i++) {
+        $row = $rows[$i];
+        $pdf->Cell(20, 10, $row['nom'], 1);
+        $pdf->Cell(20, 10, $row['prenom'], 1);
+        $pdf->Cell(20, 10, $row['pays'], 1);
+        $pdf->Cell(20, 10, $row['npa'], 1, 1);
+    }
+    $pdf->Output();
+  }
+
+  function printXML($rows){
+
+  }
+
+  function printJSON($rows){
+    echo json_encode($rows);
+  }
 ?>
